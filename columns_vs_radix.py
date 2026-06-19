82d9884fa1297269abd8ef5d4a45b6af9d93d45e
Sean Enderby <sean.enderby@gmail.com>
2026-06-19 10:12:57 +0100

    remove some junk

--- columns_vs_radix.py
+++
@@ -1,9 +0,0 @@
-from perfect_strangers import ColumnShiftMatcher
-from perfect_strangers.radix_matcher import RadixMatcher
-
-for s in range(3, 7):
-    for e in range(2, 4):
-        c = ColumnShiftMatcher(s**(e-1), s)
-        r = RadixMatcher(s, e)
-        print(f"{s}-{e}: Columns: {c.max_rounds} Radix {r.max_rounds}")
-
