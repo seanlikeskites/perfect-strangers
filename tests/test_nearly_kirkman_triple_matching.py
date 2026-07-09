8ce8878e5b57750eb0d6b975778c908ef271d76c
Sean Enderby <sean.enderby@gmail.com>
2026-07-09 15:18:27 +0100

    all starters for NKTSs given in Abel et.al.

--- tests/test_nearly_kirkman_triple_matching.py
+++ tests/test_nearly_kirkman_triple_matching.py
@@ -9,12 +9,15 @@ from perfect_strangers.util import sequence_length_upper_bound
 from tests.matcher_validation import validate_matcher
 
 
-@pytest.mark.parametrize("groups_per_round", range(8, 13, 2))
+@pytest.mark.parametrize("groups_per_round", range(8, 47, 2))
 def test_nearly_kirkman(groups_per_round):
     matcher = NearlyKirkmanTripleMatcher.create_matcher(groups_per_round)
 
-    # Nearly Kirkman triple matching should always give the maximum possible rounds.
-    assert matcher.max_rounds == sequence_length_upper_bound(groups_per_round, 3)
+    # No starters given for 114 participants in the paper. There is a construction for this case,
+    # so that's something to implement later.
+    if groups_per_round != 38:
+        # Nearly Kirkman triple matching should always give the maximum possible rounds.
+        assert matcher.max_rounds == sequence_length_upper_bound(groups_per_round, 3)
 
-    # Validate generated rounds
-    validate_matcher(matcher)
+        # Validate generated rounds
+        validate_matcher(matcher)
