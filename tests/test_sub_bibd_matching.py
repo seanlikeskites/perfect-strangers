4abfb2e6131db6976980a7c7d6c3c854518d0609
Sean Enderby <sean.enderby@gmail.com>
2026-07-24 10:17:59 +0100

    use sub bibd matching for groups of size 4 too

--- tests/test_sub_bibd_matching.py
+++ tests/test_sub_bibd_matching.py
@@ -8,14 +8,25 @@ from perfect_strangers.sub_bibd_matcher import SubBIBDMatcher
 from perfect_strangers.util import sequence_length_upper_bound
 from tests.matcher_validation import validate_matcher
 
+test_cases = [
+    # Groups of 3
+    (11, 3),
+    (15, 3),
+    (19, 3),
+    (21, 3),
+    (27, 3),
 
-@pytest.mark.parametrize("groups_per_round", [11, 15, 19, 21, 27])
-def test_sub_bibd(groups_per_round):
-    matcher = SubBIBDMatcher.create_matcher(groups_per_round, 3, [])
+    # Groups of 4
+    (16, 4),
+    (19, 4)
+]
 
-    if matcher is not None:
-        # Sub-BIBD matching should always give the maximum possible rounds.
-        assert matcher.max_rounds == sequence_length_upper_bound(groups_per_round, 3)
+@pytest.mark.parametrize(("groups_per_round", "group_size"), test_cases)
+def test_sub_bibd(groups_per_round, group_size):
+    matcher = SubBIBDMatcher.create_matcher(groups_per_round, group_size, [])
 
-        # Validate generated rounds
-        validate_matcher(matcher)
+    # Sub-BIBD matching should always give the maximum possible rounds.
+    assert matcher.max_rounds == sequence_length_upper_bound(groups_per_round, group_size)
+
+    # Validate generated rounds
+    validate_matcher(matcher)
